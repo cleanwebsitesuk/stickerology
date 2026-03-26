e1b8294cbe8f8b24cfd3281fd2fb38f688313691
Clean Websites <cleanwebsitesuk@gmail.com>
2026-03-26 20:22:54 +0000

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "Stickerology",
   description: "World-class stickers",
+  icons: {
+    icon: "/favicon.png",
+  },
 };
 
 export default function RootLayout({
