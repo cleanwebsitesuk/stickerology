193c0706b030b47520e672f95ee3982286a4bb26
Clean Websites <cleanwebsitesuk@gmail.com>
2026-03-29 21:33:07 +0100

    Update shop-add-button.tsx

--- components/shop-add-button.tsx
+++ components/shop-add-button.tsx
@@ -15,7 +15,6 @@ type Props = {
 
 export default function ShopAddButton({ product }: Props) {
   const { addItem } = useCart();
-
   const outOfStock = product.quantity <= 0;
 
   return (
@@ -35,7 +34,7 @@ export default function ShopAddButton({ product }: Props) {
       }
       className="inline-flex items-center rounded-full border border-neutral-300 bg-white px-4 py-2 text-sm font-semibold text-neutral-800 transition hover:border-[#BC2229]/35 hover:text-[#BC2229] disabled:cursor-not-allowed disabled:opacity-50"
     >
-      {outOfStock ? "Out of Stock" : "Add to Cart"}
+      {outOfStock ? "Out of stock" : "Add to Cart"}
     </button>
   );
 }
