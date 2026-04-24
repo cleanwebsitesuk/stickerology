cc22a5a60f054692a33b3ec670488654cd1a13ea
Clean Websites <cleanwebsitesuk@gmail.com>
2026-04-24 20:09:03 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { CartProvider } from "@/context/cart-context";
 import SiteHeader from "@/components/site-header";
 import SiteFooter from "@/components/site-footer";
 
@@ -34,13 +33,11 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full bg-white text-neutral-950">
-        <CartProvider>
-          <div className="flex min-h-screen flex-col">
-            <SiteHeader />
-            {children}
-            <SiteFooter />
-          </div>
-        </CartProvider>
+        <div className="flex min-h-screen flex-col">
+          <SiteHeader />
+          {children}
+          <SiteFooter />
+        </div>
       </body>
     </html>
   );
